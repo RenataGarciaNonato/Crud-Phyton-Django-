eac356a297cb7f3132c960c6a6f9676e5dacedd0
Renata Garcia Nonato <80370151+RenataGarciaNonato@users.noreply.github.com>
2021-09-15 23:21:47 -0300

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from app.models import Pacientes
-
-class PacientesForm(ModelForm):
-     class Meta:
-         model = Pacientes
-         fields = ['Nome', 'CPF', 'Cirurgia']
